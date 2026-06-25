0ccb7950a24c2333cdf070ffdcb9e36e64faf303
qizonggui-cmyk <qizonggui@gmail.com>
2026-06-25 18:47:04 +0800

    fix pc api base url fallback

--- template/pc/setting.js
+++ template/pc/setting.js
@@ -1,10 +1,13 @@
 // 请求接口地址 根据环境判断
 // 接口配置 在根目录 env.js 进行配置
-let VUE_APP_API_URL
+let VUE_APP_API_URL = '';
+const envConfig = process.env.NUXT_ENV || {};
 
 // 默认打包设置，如果使用SEO打包，请注释下面三行代码
 if (process.browser) {
-  VUE_APP_API_URL = `${process.env.NUXT_ENV.VUE_APP_API_URL}` || `${location.origin}/api`;
+  VUE_APP_API_URL = envConfig.VUE_APP_API_URL || `${location.origin}/api`;
+} else {
+  VUE_APP_API_URL = envConfig.VUE_APP_API_URL || '';
 }
 // SEO打包设置，如果使用SEO打包，请将下方代码打开
 // VUE_APP_API_URL = '您的域名/api';
